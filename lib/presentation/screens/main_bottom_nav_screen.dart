0b2f03d282398fce6ee78706380b7cc99f1c871e
kdrbipul <kdrbipul@gmail.com>
2024-02-28 15:47:59 +0600

    nav bar add and some correction

---
+++ lib/presentation/screens/main_bottom_nav_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_project/presentation/utils/app_color.dart';
+
+class MainBottomNavScreen extends StatefulWidget {
+  const MainBottomNavScreen({super.key});
+
+  @override
+  State<MainBottomNavScreen> createState() => _MainBottomNavScreenState();
+}
+
+class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
+
+  int _currentSelectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentSelectedIndex,
+        selectedItemColor: AppColor.themeColor,
+        unselectedItemColor: AppColor.secondaryTextColor,
+        showUnselectedLabels: true,
+        onTap: (index){
+          _currentSelectedIndex = index;
+          if (mounted){
+            setState(() {});
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_copy_outlined),
+            label: 'New Task'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done_all),
+            label: 'Complete'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_rounded),
+            label: 'Progress'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.close_rounded),
+            label: 'Cancelled'
+          ),
+        ],
+      ),
+
+    );
+  }
+}
--- lib/presentation/screens/sign_in_screen.dart
+++ lib/presentation/screens/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_project/presentation/screens/email_verification_screen.dart';
+import 'package:task_manager_project/presentation/screens/main_bottom_nav_screen.dart';
 import 'package:task_manager_project/presentation/screens/sign_up_screen.dart';
 import 'package:task_manager_project/presentation/utils/app_color.dart';
 import 'package:task_manager_project/presentation/widgets/bg_image_screen.dart';
@@ -69,7 +70,14 @@ class _SignInScreenState extends State<SignInScreen> {
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainBottomNavScreen(),
+                            ),
+                            (route) => false);
+                      },
                       child: const Icon(Icons.arrow_circle_right_outlined),
                     ),
                   ),
