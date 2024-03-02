555101027f8f981c6b466ba522421f25d893a66c
kdrbipul <kdrbipul@gmail.com>
2024-03-02 16:34:40 +0600

    new file add

---
+++ lib/presentation/screens/update_profile_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_project/presentation/widgets/bg_image_screen.dart';
+import 'package:task_manager_project/presentation/widgets/profile_bar.dart';
+
+class UpdateProfileScreen extends StatefulWidget {
+  const UpdateProfileScreen({super.key});
+
+  @override
+  State<UpdateProfileScreen> createState() => _UpdateProfileScreenState();
+}
+
+class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _phoneTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isObsecureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: profileAppBar,
+      body: BgImageScreen(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 48,
+                  ),
+                  Text(
+                    'Update Profile',
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          fontSize: 24,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: const BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(8),
+                                topLeft: Radius.circular(8),
+                              )),
+                          child: const Text(
+                            'Photo',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        const Text('image.png'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _emailTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                      labelText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _firstNameTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'First Name',
+                      labelText: 'First Name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _lastNameTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Last Name',
+                      labelText: 'Last Name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _phoneTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Mobile',
+                      labelText: 'Mobile',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _passwordTEController,
+                    obscureText: _isObsecureText,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      labelText: 'Password',
+                      suffixIcon: togglePassword(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(Icons.arrow_circle_right_outlined),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  IconButton togglePassword() {
+    return IconButton(
+      onPressed: () {
+        _isObsecureText = !_isObsecureText;
+        setState(() {});
+      },
+      icon: Icon(_isObsecureText ? Icons.visibility_off : Icons.visibility),
+      color: Colors.grey,
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _phoneTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
+}
--- lib/presentation/widgets/profile_bar.dart
+++ lib/presentation/widgets/profile_bar.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_project/app.dart';
 import 'package:task_manager_project/presentation/screens/auth/sign_in_screen.dart';
+import 'package:task_manager_project/presentation/screens/update_profile_screen.dart';
 import 'package:task_manager_project/presentation/utils/app_color.dart';
 
 PreferredSizeWidget get profileAppBar {
   return AppBar(
     automaticallyImplyLeading: false,
     backgroundColor: AppColor.themeColor,
-    title: Row(
-      children: [
-        const CircleAvatar(),
-        const SizedBox(width: 10.0),
-        const Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Abdul Kader',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              Text(
-                'example@gmail.com',
-                style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
-              ),
-            ],
+    title: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          TaskManager.navigatorKey.currentState!.context,
+          MaterialPageRoute(
+            builder: (context) => const UpdateProfileScreen(),
           ),
-        ),
-        IconButton(
-          color: Colors.white,
-          onPressed: () {
-            Navigator.pushAndRemoveUntil(
-                TaskManager.navigatorKey.currentState!.context,
-                MaterialPageRoute(
-                  builder: (contex) => const SignInScreen(),
+        );
+      },
+      child: Row(
+        children: [
+          const CircleAvatar(),
+          const SizedBox(width: 10.0),
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Abdul Kader',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
-                (route) => false);
-          },
-          icon: const Icon(
-            Icons.logout_outlined,
+                Text(
+                  'example@gmail.com',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          IconButton(
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  TaskManager.navigatorKey.currentState!.context,
+                  MaterialPageRoute(
+                    builder: (contex) => const SignInScreen(),
+                  ),
+                  (route) => false);
+            },
+            icon: const Icon(
+              Icons.logout_outlined,
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
